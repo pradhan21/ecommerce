c0649da11569f053fc7d5f14f219456e1b1a5644
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2022-05-29 10:47:31 +0545

    lab3

--- ecommerce_lab1/ecommerce_lab1/settings.py
+++ ecommerce_lab1/ecommerce_lab1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product_module',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INSTALLED_APPS = [ ...,
-'product_module' ]
--- ecommerce_lab1/product_module/admin.py
+++ ecommerce_lab1/product_module/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Brand
+from .models import Brand, Category, Product
 admin.site.register(Brand)
+admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["image_tag", "name", "price", "brand", "category",]
+    search_fields = ["name", "price", "brand__name", "category__name",]
+    list_filter = ["brand","category","price",]
+    # readonly_fields = ["quantity",]
+    class Meta:
+        model = Product
+admin.site.register(Product, ProductAdmin)
+
+
+####class CategoryAdmin(admin.ModelAdmin):
+    #list_display = ["image_tag", "name", "price", "brand", "category",]
+    ##list_filter = ["brand","category","price",]
+    # readonly_fields = ["quantity",]
+    #class Meta:
+     #   model = Product
+#admin.site.register(Product, ProductAdmin)###
